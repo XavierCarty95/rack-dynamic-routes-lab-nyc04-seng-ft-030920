34faae6d067bb9f194c87c47bf9a8b7f91d0f7ce
Xavier Carty <xaviercarty@gmail.com>
2020-03-30 03:31:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,13 @@
 class Application 
   
+  @@items = [Items.new("Apples" , 4.23), Items.new("Oranges", 3.23)]
   def call(env)
   
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
     
-    req.path.
+    req.path.match(/item/)
   
   end
   
